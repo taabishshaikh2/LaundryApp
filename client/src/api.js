de4c8600d7b5f1380466d3fa86026b397382b74f
taabishshaikh2 <taabishshaikh2@gmail.com>
2026-09-18 20:18:36 +0530

    pilot test launch to render

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("dg_token");
+
   if (token) {
     config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
